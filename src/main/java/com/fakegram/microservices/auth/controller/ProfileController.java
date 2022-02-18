b43e07f43b7374ea7fe73c2b01b53a5826a0b877
Mohamed Rezk <mohamed.rezk@orbyta.it>
2022-02-18 17:40:26 +0100

    added ProfileUtils methods

---
+++ src/main/java/com/fakegram/microservices/auth/controller/ProfileController.java
@@ -0,0 +1,8 @@
+package com.fakegram.microservices.auth.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfileController {
+
+}
--- src/main/java/com/fakegram/microservices/auth/utils/ProfileUtils.java
+++ src/main/java/com/fakegram/microservices/auth/utils/ProfileUtils.java
@@ -5,16 +5,26 @@ import com.fakegram.microservices.auth.entity.Profile;
 
 public class ProfileUtils {
 	public static ProfileDTO profileEntityToProfileDTO(Profile profile) {
-		/**
-		 * TODO
-		 */
-		return null;
+		ProfileDTO profileDTO = new ProfileDTO();
+		profileDTO.setBio(profile.getBio());
+		profileDTO.setEmail(profile.getEmail());
+		profileDTO.setId(profile.getIdProfile());
+		profileDTO.setName(profile.getName());
+		profileDTO.setNickname(profile.getNickname());
+		profileDTO.setProPic(profile.getProPic());
+		
+		return profileDTO;
 	}
 	
 	public static Profile profileDTOToProfileEntity(ProfileDTO profileDTO) {
-		/**
-		 * TODO
-		 */
-		return null;
+		Profile profile = new Profile();
+		profile.setBio(profileDTO.getBio());
+		profile.setEmail(profileDTO.getEmail());
+		profile.setIdProfile(profileDTO.getId());
+		profile.setName(profileDTO.getName());
+		profile.setNickname(profileDTO.getNickname());
+		profile.setProPic(profileDTO.getProPic());
+		
+		return profile;
 	}
 }
